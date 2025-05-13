634f67fe4102176b79d658eedcf5d30d85a041b6
lurzua <lu.urzua@duocuc.cl>
2025-05-13 09:56:44 -0400

    perdón

---
+++ msvc-reportes/src/main/java/com/edutech/msvc/reportes/MsvcReportesApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.msvc.reportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcReportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcReportesApplication.class, args);
+	}
+
+}
