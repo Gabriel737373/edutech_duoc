bbc07f2e3417a4b6d228fd1a4027d5a7e67ab0ed
Ariel <ari.molinam@duocuc.cl>
2025-05-13 10:21:20 -0400

    Commit deje la caga Curso

--- msvc-curso/src/main/java/com/edutech/msvc/curso/controllers/CursoController.java
+++ msvc-curso/src/main/java/com/edutech/msvc/curso/controllers/CursoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v2/cursos")
+@RequestMapping("/api/v2/cursos")
 @Validated
 public class CursoController {
 
