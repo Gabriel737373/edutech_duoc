9b006670767ba89596b7adf53c57acce2273e1da
Ariel <ari.molinam@duocuc.cl>
2025-05-12 20:03:03 -0400

    Commit AlumnoException msvc

---
+++ msvc-alumnos/src/main/java/com/edutech/msvc/alumno/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.edutech.msvc.alumno.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+// La verdad no se que hace esto pero queria que se vieran bien los errores en el Postman asdfsdf no supe hacerlo asi que lo copie
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String campo = ((FieldError) error).getField();
+            String mensaje = error.getDefaultMessage();
+            errores.put(campo, mensaje);
+        });
+
+        return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AlumnoException.class)
+    public ResponseEntity<Map<String, String>> handleAlumnoException(AlumnoException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+}
