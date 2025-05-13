80e5b516d96be58fdb9f40b10bce2bec620f59e9
jonaour <jo.naour@duocuc.cl>
2025-05-13 10:53:41 -0400

    Commit msvc-resenias

---
+++ msvc-resenias/src/test/java/com/edutech/msvc/resenias/MsvcReseniasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.msvc.resenias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcReseniasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
