df653fe56cbb826d06cd1d27fd6647ac5506a7bb
jonaour <jo.naour@duocuc.cl>
2025-05-13 23:51:46 -0400

    Model Resenias

---
+++ msvc-resenias/src/main/java/com/edutech/msvc/resenias/models/entities/Resenias.java
@@ -0,0 +1,24 @@
+package com.edutech.msvc.resenias.models.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Table(name="resenias")
+@Getter @Setter @ToString
+@NoArgsConstructor @AllArgsConstructor
+public class Resenias {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_resenia")
+    private Long idResenia;
+
+    @Column(name = "comentario_resenia")
+    private String comentarioResenia;
+
+    @Column(name = "valoracion_resenia")
+    @NotNull(message = "El campo valoración reseña no puede estar vacío")
+    private Integer valoricacionResenia;
+}
