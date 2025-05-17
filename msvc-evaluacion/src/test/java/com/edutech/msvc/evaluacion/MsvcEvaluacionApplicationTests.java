03346d41f75478d233c80c59b0c095df426d64ff
gabri <ga.velasquezm@duocuc.cl>
2025-05-17 14:57:53 -0400

    init commit
    Signed-off-by: gabri <ga.velasquezm@duocuc.cl>

---
+++ msvc-evaluacion/src/test/java/com/edutech/msvc/evaluacion/MsvcEvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.msvc.evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcEvaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
