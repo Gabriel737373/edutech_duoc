e8ec8e4ec20f4ddf0a54b2d354c6a29f8e7d2f21
jonaour <jo.naour@duocuc.cl>
2025-05-21 20:52:07 -0400

    Commit add MSVC-inscripcionCurso

---
+++ msvc-inscripcionCurso/src/main/java/com/jonaour/msvc/inscripcionCurso/MsvcInscripcionCursoApplication.java
@@ -0,0 +1,13 @@
+package com.jonaour.msvc.inscripcionCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcInscripcionCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcInscripcionCursoApplication.class, args);
+	}
+
+}
---
+++ msvc-inscripcionCurso/src/test/java/com/jonaour/msvc/inscripcionCurso/MsvcInscripcionCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonaour.msvc.inscripcionCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcInscripcionCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
