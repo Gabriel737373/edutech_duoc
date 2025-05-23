cca88449ed1b1ab617d126e9583304a98cfd8021
gabri <ga.velasquezm@duocuc.cl>
2025-05-23 13:06:34 -0400

    init commit
    Signed-off-by: gabri <ga.velasquezm@duocuc.cl>

--- msvc-profesor/src/main/java/com/edutech_duoc/msvc_profesor/MsvcProfesorApplication.java
+++ msvc-profesor/src/main/java/com/edutech_duoc/msvc_profesor/MsvcProfesorApplication.java
@@ -2,8 +2,10 @@ package com.edutech_duoc.msvc_profesor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsvcProfesorApplication {
 
 	public static void main(String[] args) {
