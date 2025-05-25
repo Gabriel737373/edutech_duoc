a8c750ad30d560448724ebb3a908bc1f276814d4
lucasfrrada <lu.urzua@duocuc.cl>
2025-05-24 20:54:19 -0400

    Prueba Gerente de cursos

---
+++ msvc-gerenteCurso/src/main/java/com/edutech/msvc/gerenteCurso/MsvcGerenteCursoApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.msvc.gerenteCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcGerenteCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcGerenteCursoApplication.class, args);
+	}
+
+}
---
+++ msvc-gerenteCurso/src/test/java/com/edutech/msvc/gerenteCurso/MsvcGerenteCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.msvc.gerenteCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGerenteCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
