13c15aca40f8444b3371f65c9f0f6bbd53ee233b
gabri <ga.velasquezm@duocuc.cl>
2025-06-19 09:55:38 -0400

    avanze en los test de profesor
    Signed-off-by: gabri <ga.velasquezm@duocuc.cl>

---
+++ msvc-profesor/src/test/java/com/edutech_duoc/msvc_profesor/controller/ProfesorControllerTest.java
@@ -0,0 +1,30 @@
+package com.edutech_duoc.msvc_profesor.controller;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProfesorControllerTest {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void shouldReturnAllCursosWhenListIsRequested(){
+
+        ResponseEntity<String> response = testRestTemplate.getForEntity("/api/v2/profesores", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        int profesoresCount = documentContext.read("$.length()");
+        assertThat(profesoresCount).isEqualTo(1000);
+    }
+}
