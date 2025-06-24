5072dcf30965b4c8380c744ac39759babb46223e
jonaour <jo.naour@duocuc.cl>
2025-06-24 10:05:47 -0400

    ADD assembler InscripcionCurso

--- msvc-inscripcionCurso/src/main/java/com/jonaour/msvc/inscripcionCurso/assemblers/InscripcionCursoModelAssembler.java
+++ msvc-inscripcionCurso/src/main/java/com/jonaour/msvc/inscripcionCurso/assemblers/InscripcionCursoModelAssembler.java
@@ -19,7 +19,9 @@ public class InscripcionCursoModelAssembler implements RepresentationModelAssemb
                 linkTo(methodOn(InscripcionCursoControllerV2.class).findById(entity.getIdInscripcionCurso())).withSelfRel(),
                 linkTo(methodOn(InscripcionCursoControllerV2.class).findAll()).withRel("inscripcionCursos"),
                 Link.of("http://localhost:8081/api/v2/alumnos/"+entity.getIdAlumno()).withRel("alumnos"),
-                Link.of("http://localhost:8084/api/v2/cursos/"+entity.getIdCurso()).withRel("cursos")
+                Link.of("http://localhost:8084/api/v2/cursos/"+entity.getIdCurso()).withRel("cursos"),
+                Link.of("http://localhost:8088/api/v2/gerenteCursos/"+entity.getIdGerenteCurso()).withRel("gerenteCursos"),
+                Link.of("http://localhost:8083/api/v2/profesores/"+entity.getIdProfesor()).withRel("profesores")
         );
     }
 }
