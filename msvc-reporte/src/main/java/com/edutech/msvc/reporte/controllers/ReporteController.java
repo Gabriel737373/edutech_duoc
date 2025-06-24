0cb78e7b3b25f807fb49054ad14d825c8d1c5896
jonaour <jo.naour@duocuc.cl>
2025-06-23 20:59:16 -0400

    ADD reporte Test/Swagger/ControllerV2

--- msvc-reporte/src/main/java/com/edutech/msvc/reporte/controllers/ReporteController.java
+++ msvc-reporte/src/main/java/com/edutech/msvc/reporte/controllers/ReporteController.java
@@ -1,8 +1,16 @@
 package com.edutech.msvc.reporte.controllers;
 
+import com.edutech.msvc.reporte.dtos.ErrorDTO;
 import com.edutech.msvc.reporte.dtos.ReporteDTO;
 import com.edutech.msvc.reporte.models.entities.Reporte;
 import com.edutech.msvc.reporte.services.ReporteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,11 +31,33 @@ public class ReporteController {
     private ReporteService reporteService;
 
     @GetMapping
+    @Operation(
+            summary = "Devuelve todos los reportes de curso",
+            description = "Este metodo retorna una lista de reportes. En caso "+
+                    "de que no encuentre nada, retornara una lista vacía"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se retornaros todos los reportes")
+    })
     public ResponseEntity<List<Reporte>> findAll() {
-        return ResponseEntity.status(HttpStatus.OK).body(reporteService.findAll());
+        List<Reporte> reportes = this.reporteService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(reportes);
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Devuelve un reporte respecto a su ID",
+            description = "Este método retorna un reporte cuando es consultado mediante su ID"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se retorna el reporte encontrado"),
+            @ApiResponse(responseCode = "404", description = "Error - Reporte con ID no se encuentra",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDTO.class)))
+    })
+    @Parameters(value = {
+            @Parameter(name = "id",description = "Este es el ID único de un reporte", required = true)
+    })
     public ResponseEntity<Reporte> findById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(reporteService.findById(id));
     }
@@ -38,6 +68,22 @@ public class ReporteController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "Endpoint que permite guardar un reporte",
+            description = "Este endpoint manda un body con el formato Reporte.class"+
+                    "y permite la creación de un reporte"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Reporte creado correctamente")
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            required = true,
+            description = "Este debe ser el json con los datos de una inscripcion curso",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Reporte.class)
+            )
+    )
     public ResponseEntity<Reporte> save(@Valid @RequestBody Reporte reporte) {
         return ResponseEntity.status(HttpStatus.CREATED).body(reporteService.save(reporte));
     }
@@ -51,6 +97,16 @@ public class ReporteController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Eliminar un reporte por ID",
+            description = "Este endpoint permite eliminar un reporte existente a través de su ID"+
+                    "Si el reporte es eliminado exitosamente, retorna un código 204 sin contenido"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Reporte eliminado correctamente"),
+            @ApiResponse(responseCode = "404", description = "Error - Reporte con ID no encontrado en la base de datos",
+                    content = @Content)
+    })
     public ResponseEntity<Reporte> delete(@PathVariable Long id) {
         this.reporteService.deleteById(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
